50c870493004d6e1b1bb00e91f3d496b2c01fa08
Joe Reddington <joe@equalitytime.co.uk>
2023-11-20 15:29:10 +0000

    temp

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash 
 #Following section from https://stackoverflow.com/a/246128/170243
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd /home/joe/  #should be root # Why is this here?  
+ln -s /home/joe/git/dotvimdirectory/ /home/joe/.vim
+ln -s .vim/vimrc .vimrc
+
 cd $SCRIPT_DIR
 # Sort out submodules
 cd bundle 
 git submodule init
 git submodule update
-cd ../.. #should be root # Why is this here?  
 # Link the vimrc file 
-ln -s ~/.vim/vimrc ~/.vimrc
 
 # Some reminders
 echo "Don't forget the .bashrc files"
